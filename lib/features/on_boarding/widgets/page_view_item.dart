4a4f470fc7ba3e2e1b9e36acdb9d5f093f9cf7f9
mohammedashraf16 <mohammedashraf1692003@gmail.com>
2024-06-28 20:18:13 +0300

    Add Localization & PageView UI

---
+++ lib/features/on_boarding/widgets/page_view_item.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class PageViewItem extends StatelessWidget {
+  const PageViewItem({
+    super.key,
+    required this.image,
+    required this.backgroundImage,
+    required this.subTitle,
+    required this.title,
+  });
+
+  final String image, backgroundImage;
+  final String subTitle;
+  final Widget title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        SizedBox(
+          width: double.infinity,
+          height: MediaQuery.of(context).size.height * 0.5,
+          child: Stack(
+            children: [
+              Positioned.fill(
+                child: SvgPicture.asset(
+                  backgroundImage,
+                  fit: BoxFit.fill,
+                ),
+              ),
+              Positioned(
+                bottom: 0,
+                left: 0,
+                right: 0,
+                child: SvgPicture.asset(
+                  image,
+                ),
+              ),
+              const Padding(
+                padding: EdgeInsets.all(16.0),
+                child: Text("تخط"),
+              )
+            ],
+          ),
+        )
+      ],
+    );
+  }
+}
