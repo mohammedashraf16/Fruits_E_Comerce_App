c82820dcaae1637acceab3b803554ff5589fb05f
mohammedashraf16 <mohammedashraf1692003@gmail.com>
2024-10-30 15:33:14 +0300

    create cubit & add signUp Cubit.

---
+++ lib/features/auth/presentation/cubits/signup_cubit/signup_state.dart
@@ -0,0 +1,14 @@
+part of 'signup_cubit.dart';
+
+abstract class SignupState {}
+
+final class SignupInitial extends SignupState {}
+final class SignupLoading extends SignupState {}
+final class SignupSuccess extends SignupState {
+  final UserEntity userEntity;
+  SignupSuccess({required this.userEntity});
+}
+final class SignupFailure extends SignupState {
+  final String errorMessage;
+  SignupFailure({required this.errorMessage});
+}
